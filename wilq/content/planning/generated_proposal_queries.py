f5e104c8d99d8c2ea2ed118ecf084e800d643c96
korneliuszburian <krnijuu@gmail.com>
2026-08-31 11:31:21 +0200

    fix(content): keep planning queries static

--- wilq/content/planning/generated_proposal_queries.py
+++ wilq/content/planning/generated_proposal_queries.py
@@ -15,21 +15,32 @@ PROPOSAL_INPUT_SELECTS = {
     """,
 }
 PROPOSAL_LATEST_SELECTS = {
-    table: (
+    "content_planning_proposals": (
         "SELECT payload_json, proposal_version, work_item_id, service_card_id, "
-        "content_kind, subject_key, planning_input_digest FROM " + table + " WHERE "
-    )
-    for table in PROPOSAL_INPUT_SELECTS
+        "content_kind, subject_key, planning_input_digest "
+        "FROM content_planning_proposals WHERE "
+    ),
+    "content_planning_proposal_repairs": (
+        "SELECT payload_json, proposal_version, work_item_id, service_card_id, "
+        "content_kind, subject_key, planning_input_digest "
+        "FROM content_planning_proposal_repairs WHERE "
+    ),
 }
 PROPOSAL_PLANNING_DIGEST_SELECTS = {
-    table: (
-        "SELECT payload_json, proposal_version, work_item_id, service_card_id, "
-        "content_kind, subject_key, planning_input_digest FROM "
-        + table
-        + " WHERE work_item_id = ? "
-        "AND json_extract(payload_json, '$.planning_digest') = ?"
-    )
-    for table in PROPOSAL_INPUT_SELECTS
+    "content_planning_proposals": """
+        SELECT payload_json, proposal_version, work_item_id, service_card_id,
+               content_kind, subject_key, planning_input_digest
+        FROM content_planning_proposals
+        WHERE work_item_id = ?
+          AND json_extract(payload_json, '$.planning_digest') = ?
+    """,
+    "content_planning_proposal_repairs": """
+        SELECT payload_json, proposal_version, work_item_id, service_card_id,
+               content_kind, subject_key, planning_input_digest
+        FROM content_planning_proposal_repairs
+        WHERE work_item_id = ?
+          AND json_extract(payload_json, '$.planning_digest') = ?
+    """,
 }
 
 __all__ = [
